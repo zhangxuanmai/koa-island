44fff20ec46c7ab014c3076ac71cda494d0e90cd
zhangyuan <674775293@qq.com>
2020-05-16 17:37:22 +0800

    fix: update

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const Koa = require('koa')
+const parser = require('koa-bodyparser')
+const initManger = require('./utils/init')
+const catchError = require('./middlewares/exception')
 
-// 应用程序对象 中间件
 const app = new Koa()
 
-// 注册
-app.use(async (ctx, next) => {
-  console.log('hello')
-  await next()
-})
+// catch error message
+app.use(catchError)
+// parser body
+app.use(parser())
+// init routers
+initManger.initCore(app)
 
 app.listen(9000)
